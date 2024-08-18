2c38453afc7df1805a923130ca4183f08b926b92
Amir5793 <amienakhostin48@gmail.com>
2024-08-18 05:13:55 -0700

    rotate your phone edited

--- app/index.js
+++ app/index.js
@@ -2,43 +2,41 @@ const $ = document
 
 let amethystSound = new Audio('./music/sound effects/logo/amethyst.m4a')
 let dynamiteSound = new Audio('./music/sound effects/logo/Creeper_fuse.ogg')
-const rotateContainer = $.querySelector('.rotate-mobile-container')
+let soundEffectsFlag = false
+
+const rotateContainer = $.querySelector('.rotate-mobile-container') 
 const mobile = $.querySelector('.mobile')
+const rotateText = $.querySelector('.rotate-warning-text-container')
+const mobileTopArrow = $.querySelector('.mobile-top-arrow')
+const mobileBottomArrow = $.querySelector('.mobile-bottom-arrow')
+
 const modal = $.querySelector('.modal-container')
 const accessBtn = $.querySelector('.access-btn')
 const rejectBtn = $.querySelector('.reject-btn')
-const logo = $.querySelector('.logo-container')
-// const logoShineSvg = $.querySelector('.logo-shine')
-const logoText = $.querySelector('.logo-text')
-const loadingContainer = $.querySelector('.loading-container')
-const loadingBarText = $.querySelector('.loading-text')
-const gameText = $.querySelector('.game-text')
-const gameLogo = $.querySelector('.game-logo')
-const gameLogoFire = $.querySelector('.dynamite-fire')
-const progressBarContainer = $.querySelector('.progress-bar-container')
-const progressBar = $.querySelector('.progress-bar')
-const range = $.querySelector('.range')
 
-
-let soundEffectsFlag = false
-
-
-logo.style.display = 'none'
-loadingContainer.style.display  = 'none'
-gameText.style.display = 'none'
-progressBarContainer.style.display = 'none'
-gameLogo.style.display = 'none'
-gameLogoFire.style.display = 'none'
-
-if (rotateContainer) {
+// for 'rotate your phone' animation
+if (window.innerHeight <= 481) {
     modal.style.display = 'none'
-
     mobile.addEventListener('animationend', () => {
-        rotateContainer.style.display = 'none'
-        modal.style.display = 'inline-block'
+        rotateText.style.display = 'inline-block'
+        mobileTopArrow.style.display = 'none'
+        mobileBottomArrow.style.display = 'none'
+        setTimeout(() => {
+            rotateContainer.style.display = 'none'
+            modal.style.display = 'inline-block'
+        },2000)
     })
-} 
+} else {
+    rotateContainer.style.display = 'none'
+    modal.style.display = 'inline-block'
+}
 
+
+const logo = $.querySelector('.logo-container')
+const logoText = $.querySelector('.logo-text')
+logo.style.display = 'none'
+
+// for modal
 accessBtn.addEventListener('click', e => {
     $.body.style.backgroundColor = '#fff'
     modal.style.display = 'none'
@@ -47,7 +45,7 @@ accessBtn.addEventListener('click', e => {
     setTimeout(() => {
         logo.style.display = 'inline-block'
     },1000)
-
+    
 })
 rejectBtn.addEventListener('click', e => {
     $.body.style.backgroundColor = '#fff'
@@ -57,23 +55,42 @@ rejectBtn.addEventListener('click', e => {
     },700)
 })
 
+// for game logo to show when factory's logo animation ended
 logoText.addEventListener('animationend', e => {
     logo.style.display = 'none'
     loadingContainer.style.display  = 'block'
 })
+
+const loadingContainer = $.querySelector('.loading-container')
+const loadingBarText = $.querySelector('.loading-text')
+const gameText = $.querySelector('.game-text')
+const gameLogo = $.querySelector('.game-logo')
+const gameLogoFire = $.querySelector('.dynamite-fire')
+const progressBarContainer = $.querySelector('.progress-bar-container')
+const progressBar = $.querySelector('.progress-bar')
+const range = $.querySelector('.range')
+
+loadingContainer.style.display  = 'none'
+gameText.style.display = 'none'
+progressBarContainer.style.display = 'none'
+gameLogo.style.display = 'none'
+gameLogoFire.style.display = 'none'
+
+
+// for game logo to change location when game logo animation ended
 loadingContainer.addEventListener('animationend', () => {
     gameText.style.display = 'inline-block'
     progressBarContainer.style.display = 'inline-block'
     gameLogo.style.display = 'inline-block'
     gameLogoFire.style.display = 'inline-block'
-   soundEffectsFlag &&  dynamiteSound.play()
+    soundEffectsFlag &&  dynamiteSound.play()
 })
 progressBar.addEventListener('animationstart', () => {
-let n = 1
-let loadingText = setInterval(() => {
-    loadingBarText.innerHTML = n + '%'
-
-    if (n < 100){
+    let n = 1
+    let loadingText = setInterval(() => {
+        loadingBarText.innerHTML = n + '%'
+        
+        if (n < 100){
         n++
     }else{
         soundEffectsFlag && dynamiteSound.pause()
